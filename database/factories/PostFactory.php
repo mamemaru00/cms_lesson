2d06024bf0cb7c8b99339ff88e68d0b13d678960
mamemaru00 <k.shouhei0711@gmail.com>
2022-11-01 16:27:50 +0900

    seeder

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -3,9 +3,17 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Post;
 
 class PostFactory extends Factory
 {
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Post::class;
+
     /**
      * Define the model's default state.
      *
@@ -13,8 +21,14 @@ class PostFactory extends Factory
      */
     public function definition()
     {
+        $random_data = $this->faker->dateTimeBetween('-1year', '-1day');
         return [
-            //
+            'title' => $this->faker->realText(rand(20,50)),
+            'body' => $this->faker->realText(rand(100,200)),
+            'is_public' => $this->faker->boolean(90),
+            'published_at' => $random_data,
+            'created_at' => $random_data,
+            'updated_at' => $random_data,
         ];
     }
 }
